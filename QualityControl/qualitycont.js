79f9c28cf8081f3764b0d430805e1168a60563f5
DanielMichel03 <daniel2101026@miuegypt.edu.eg>
2023-08-10 17:40:38 +0300

    commit

--- QualityControl/qualitycont.js
+++ QualityControl/qualitycont.js
@@ -6,20 +6,47 @@ function showDiv() {
   var box = document.getElementById("viewComments");
   var button = document.getElementById("view");
   var rb=document.getElementById("report");
+  var bm=document.getElementById("manage");
 
 
     if (viewComments.style.display === "none") {
         viewComments.style.display = "block";
         viewReport.style.display="none";
-        view.style.color=" rgb(188, 112, 133)"
+        viewManage.style.display="none";
+        view.style.color=" rgb(0,0,0)"
         rb.style.color="rgb(221,167,181)";
+        bm.style.color="rgb(221,167,181)";
+
       } else {
         box.style.display = "none";
-        view.style.color="rgb(0,0,0)";
+        view.style.color="rgb(221,167,181)";
 
 
       }
 }     
+
+function showManage() {
+  var box=document.getElementById("viewManage");
+  var button=document.getElementById("manage");
+  var rep = document.getElementById("viewReport");
+var divDiv=document.getElementById("viewComments");
+  var vb=document.getElementById("view");
+  var rb=document.getElementById("report");
+
+  if (box.style.display === "none") {
+    divDiv.style.display = "none";
+    rep.style.display="none";
+    viewManage.style.display = "block";
+    button.style.color=" rgb(0,0,0)"
+    vb.style.color="rgb(221,167,181)";
+    rb.style.color="rgb(221,167,181)";
+ 
+  } else {
+    viewManage.style.display = "none";
+    button.style.color="rgb(221,167,181)";
+  }
+
+}
 
 function showReport() {
 var button = document.getElementById("report");
@@ -27,17 +54,22 @@ var vb=document.getElementById("view");
 var box = document.getElementById("viewReport");
 var divDiv=document.getElementById("viewComments");
 var vm=document.getElementById("most");
+var mb=document.getElementById("manage");
+var mv=document.getElementById("viewManage")
 
 if (box.style.display === "none") {
     box.style.display = "block";
     divDiv.style.display="none";
-    button.style.color="rgb(188, 112, 133)";
+    viewManage.style.display="none";
+    button.style.color="rgb(0,0,0)";
     vb.style.color="rgb(221,167,181)";
     vm.style.color="rgb(221,167,181)";
+    mb.style.color="rgb(221,167,181)";
+
   } else {
     box.style.display = "none";
     box.style.textDecoration="none";
-    button.style.color="black";
+    button.style.color="rgb(221,167,181)";
   }
 
 }   
@@ -57,27 +89,12 @@ function showReview() {
       } else {
         star.style.display = "none";
 
-        star.style.color="black";
+        star.style.color="rgb(188, 112, 133)";
       }
     }   
 
-    function showManage() {
-      var button=document.getElementById("manage");
-      var vb=document.getElementById("view");
-      var rb=document.getElementById("report");
-
-      if (button.style.display === "none") {
-        button.style.color="rgb(188, 112, 133)";
-        vb.style.color="rgb(221,167,181)";
-        rb.style.color="rgb(221,167,181)";
-     
-      } else {
-        button.style.display = "none";
-
-        button.style.color="black";
-      }
-
-    }
+ 
+    
 
 
     function showMost() {
